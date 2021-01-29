d3b85c5ae2bd76aae6bc8055c69c9dc54dd052df
Branyes N.A <branyernunez@gmail.com>
2021-01-28 20:35:10 -0400

    Cambios

---
+++ src/Input.js
@@ -0,0 +1,11 @@
+import { useState } from "react";
+
+const Input = () =>{
+    return (
+        <div className="App">
+         <input type="text"/>
+        </div>
+      );
+    }
+    
+    export default Input;
